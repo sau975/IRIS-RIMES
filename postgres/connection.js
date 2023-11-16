965d40dc533b8fc0431d0694bd0d200f88e6bd48
DODIYAM PAVAN KUMAR <68941005+PAVANNANI007@users.noreply.github.com>
2023-11-16 11:11:04 +0530

    front-end edited

--- postgres/connection.js
+++ postgres/connection.js
@@ -1,11 +1,19 @@
 const { Client } = require('pg');
 
+// const client = new Client( {
+//   host:"localhost",
+//   user: "postgres",
+//   port: 5433,
+//   password: "Saurav@132",
+//   database: "IRIS"
+// });
 const client = new Client( {
   host:"localhost",
   user: "postgres",
-  port: 5433,
-  password: "Saurav@132",
+  port: 5432,
+  password: "Pavan@123",
   database: "IRIS"
 });
+
 
 module.exports = client;
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { NormalMapComponent } from './normal-map/normal-map.component';
 import { DepartureMapComponent } from './departure-map/departure-map.component';
 import { FrontPageComponent } from './front-page/front-page.component';
+import { DailyMapComponent } from './daily-map/daily-map.component';
 
 const routes: Routes = [
   { path: '', component: FrontPageComponent, children: [
     { path: 'departure', component: DepartureMapComponent },
     { path: 'normal', component: NormalMapComponent },
+    { path: 'daily', component: DailyMapComponent },
     { path: '', redirectTo: 'departure', pathMatch: 'full' }
   ]}
 ];
