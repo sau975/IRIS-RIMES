af0c347b6fa0b4fd912939f563c05a999bac4d15
Saurav Kumar <sau975>
2023-11-17 16:11:24 +0530

    login page

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -7,13 +7,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
   username: string | undefined;
-  password: string| undefined;
+  password: string | undefined;
+
+  users = [
+    {id: 101, username: "rimes@int", password: "123"},
+    {id: 102, username: "pawan@int", password: "123"},
+    {id: 103, username: "saurav@int", password: "123"}
+  ];
 
   constructor(private router: Router) { }
 
   login() {
-    this.router.navigate(['/front-page']);
+    let isAuthorised = this.users.find(x => x.username == this.username && x.password == this.password);
+    if(isAuthorised){
+      this.router.navigate(['/front-page']);
+    }else{
+      alert("Username and Passwrod are Invalid")
+    }
   }
 
 }
