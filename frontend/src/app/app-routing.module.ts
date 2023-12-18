a49f251c0aa9a8b92efcead7045b0c1cfd543db8
Saurav Kumar <sau975>
2023-12-18 12:38:20 +0530

    map full screen issue fixed

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,14 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login/login.component';
 import { DataentryComponent } from './main_page/dataentry/dataentry.component';
 import { StationLevelDataComponent } from './station-level-data/station-level-data.component';
+import { FrontPageComponent } from './main_page/front-page/front-page.component';
+import { AuthGuard } from './auth-guard';
+import { DepartureMapComponent } from './main_page/departure-map/departure-map.component';
+import { NormalMapComponent } from './main_page/normal-map/normal-map.component';
+import { DailyMapComponent } from './main_page/daily-map/daily-map.component';
+
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'data-entry', component: DataentryComponent },
-  { path: 'station-level-data', component: StationLevelDataComponent },
-  { path: 'front-page', loadChildren: () => import('./main_page/main-page.module').then(m => m.MainPageModule)},
+  { path: 'data-entry', component: DataentryComponent, canActivate: [AuthGuard] },
+  { path: 'station-level-data', component: StationLevelDataComponent, canActivate: [AuthGuard] },
+  { path: 'front-page', component: FrontPageComponent, canActivate: [AuthGuard], children:
+    [
+      { path: 'departure', component: DepartureMapComponent },
+      { path: 'normal', component: NormalMapComponent },
+      { path: 'daily', component: DailyMapComponent },
+      { path: '', redirectTo: 'departure', pathMatch: 'full' }
+    ]
+  },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'dataentry', component: DataentryComponent }
 ];
 
 @NgModule({
