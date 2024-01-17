a5e1b8b4e57b0647e824dd0233772e49e5abf651
Saurav Kumar <sau975>
2024-01-17 11:41:14 +0530

    date issue fixed

--- frontend/src/app/main_page/front-page/front-page.component.ts
+++ frontend/src/app/main_page/front-page/front-page.component.ts
@@ -12,12 +12,14 @@ export class FrontPageComponent implements OnInit{
 
   date: string = String(new Date().getDate());
   month: string = String((new Date().getMonth()+1).toString().length == 1 ? ('0' + (new Date().getMonth()+1)) : (new Date().getMonth()+1));
+  allDaysInMonth:any[]=[];
 
   constructor(
     private router: Router,
     private dataService: DataService
     ) {
       // this.setDateMonth();
+      this.getAllDaysInMonth();
     }
 
   ngOnInit(): void {}
@@ -40,5 +42,25 @@ export class FrontPageComponent implements OnInit{
       month: this.month
     }
     this.dataService.setValue(JSON.stringify(data));
+    this.getAllDaysInMonth();
   }
+
+  getAllDaysInMonth() {
+    this.allDaysInMonth = [];
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = Number(this.month)-1;
+
+    // Set the date to the first day of the month
+    const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
+
+    // Get the last day of the month
+    const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
+
+    // Loop through each day of the month
+    for (let day = firstDayOfMonth.getDate(); day <= lastDayOfMonth.getDate(); day++) {
+      this.allDaysInMonth.push(day);
+    }
+  }
+
 }
