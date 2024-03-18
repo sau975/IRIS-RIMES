54f90c58fe3baf3e6820d0f9a9bc4bac56aa5d9b
Saurav Kumar <sau975>
2024-03-18 11:22:11 +0530

    email

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -22,13 +22,13 @@ export class AppComponent {
 
   sendEmail(){
     // if (confirm("Do want to send email") == true) {
-      let emails = ["saurav97531@gmail.com", "pavan@rimes.int", "dominic@rimes.int", "tarakesh@rimes.int", "saipraveen@rimes.int", "saurabh@rimes.int"];
-      // let emails = ["saurav97531@gmail.com"];
+      // let emails = ["saurav97531@gmail.com", "pavan@rimes.int", "dominic@rimes.int", "tarakesh@rimes.int", "saipraveen@rimes.int", "saurabh@rimes.int"];
+      let emails = ["saurav97531@gmail.com"];
       emails.forEach(email => {
         let data = {
           to: email,
           subject: `Rainfall data not received for - ${new Date().toDateString()}`,
-          text: `Hi Rainfall data not received for - ${new Date().toDateString()} please enter data`,
+          text: `Hi Rainfall data not received for - ${new Date().toDateString()} please enter data. Note: This mail is being sent for testing purposes only.`,
         }
         this.dataService.sendEmail(data).subscribe(res => {
           console.log("Email Sent Successfully");
@@ -41,7 +41,7 @@ export class AppComponent {
     // Get current time
     var now = new Date();
     // Set desired time (in this case, 11:00 AM)
-    var desiredTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 11, 0, 0, 0);
+    var desiredTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12, 0, 0, 0);
     var delay = desiredTime.getTime() - now.getTime();
 
     if (delay < 0) {
