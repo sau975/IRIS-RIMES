85e7bda5e7529d66f4c6497ca08ab22b2ac7c9bf
ghanshyam-pal <ghanshyampal789@gmail.com>
2024-05-13 13:50:50 +0530

    move slider to top

--- frontend/src/app/main_page/front-page/front-page.component.ts
+++ frontend/src/app/main_page/front-page/front-page.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import 'jspdf-autotable';
 import { Router } from '@angular/router';
 import { DataService } from 'src/app/data.service';
+import * as L from 'leaflet';
+import 'leaflet.fullscreen';
+
 
 @Component({
   selector: 'app-front-page',
@@ -16,17 +21,29 @@ export class FrontPageComponent implements OnInit {
 
   fromDate: Date = new Date();
   toDate: Date = new Date();
+  mapTileTypes: string[] = ['District'];
+  private initialZoom = 4;
+  intervalId :any;
+  slidingNo = 0;
+  currentSlide = 'INDIA_COUNTRY';
+  currentSlideName:String= "";
+  isSlider = true;
+  private slidingMap: L.Map = {} as L.Map;
+
   // allDaysInMonth:any[]=[];
 
   constructor(
     private router: Router,
+    private http: HttpClient,
     private dataService: DataService
   ) {
     // this.setDateMonth();
     // this.getAllDaysInMonth();
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    // this.slidingFunction();
+   }
 
   setFromAndToDate() {
     let data = {
@@ -45,6 +62,64 @@ export class FrontPageComponent implements OnInit {
       this.fromDate = toDate;
     }
   }
+
+  private initMap(): void {
+
+    this.slidingMap = L.map('slidingMap', {
+      center: [24, 76.9629],
+      zoom: this.initialZoom,
+      scrollWheelZoom: false,
+    });
+  }
+
+  
+  toggleSlider(): void {
+    this.isSlider = !this.isSlider
+}
+currentSlidingLayer:any;
+  loadSlidingGeoJSON(): void {
+    if (this.currentSlidingLayer) {
+      this.slidingMap.removeLayer(this.currentSlidingLayer);
+    }
+    this.http.get(`assets/geojson/${this.currentSlide}.json`).subscribe((stateRes: any) => {
+      const newSlidingLayer = L.geoJSON(stateRes, {
+        style: {
+          weight: 1,
+          opacity: 1,
+          color: 'blue',
+          fillOpacity: 0
+        }
+      });
+      newSlidingLayer.addTo(this.slidingMap);
+      this.currentSlidingLayer = newSlidingLayer;
+    });
+  }
+
+  slidingList : {id:number,region : string, lat:number, long:number,initZoom:number,name:String}[] = [
+    {id:0 , region:'INDIA_COUNTRY',lat:24,long:77,initZoom:4, name:""},
+    {id:1 , region:'regions/EAST_AND_NORTH_EAST_INDIA',lat:24,long:87,initZoom:5, name:"EAST AND NORTH EAST INDIA"},
+    {id:2 , region:'regions/NORTH_WEST_INDIA',lat:29,long:77,initZoom:5, name:"NORTH WEST INDIA"},
+    {id:3 , region:'regions/SOUTH_PENINSULA',lat:17,long:77,initZoom:5, name:"SOUTH PENINSULA"},
+    {id:4 , region:'regions/C_India',lat:22,long:77,initZoom:5, name:"CENTERAL INDIA"},
+  ]
+
+  slidingFunction(): void {
+
+    this.intervalId = setInterval(() => {
+      const data = this.slidingList.find((d)=>d.id === this.slidingNo )
+      this.currentSlide = data?.region || '';
+      this.currentSlideName = data?.name || "";
+      this.slidingMap.setView([data?.lat||24,data?.long||77],data?.initZoom||4);
+      this.loadSlidingGeoJSON();
+      if(this.slidingNo<4){
+        this.slidingNo = this.slidingNo + 1
+      }else{
+        this.slidingNo = 0;
+      }
+      console.log("interval function " +  this.currentSlide);
+    }, 5000); // 5000 milliseconds = 5 seconds
+  }
+
 
 
 }
