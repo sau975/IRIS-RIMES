2c4e976a1f9ab82a3d9143de7f14d3ecf2e1471e
DODIYAM PAVAN KUMAR <dodiyampavankumar2001@gmail.com>
2024-05-16 14:39:39 +0530

    updates

--- backend/connection.js
+++ backend/connection.js
@@ -1,50 +1,12 @@
 const { Client } = require('pg');
 
-// const client = new Client( {
-//   host:"localhost",
-//   user: "postgres",
-//   port: 5433,
-//   password: "Saurav@132",
-//   database: "IRIS"
-// });
 
-<<<<<<< HEAD
-=======
-// const client = new Client( {
-//   host:"localhost",
-//   user: "postgres",
-//   port: 5432,
-//   password: "iru@ADMIN123",
-//   database: "iru_imd_db"
-// });
-<<<<<<< HEAD
-=======
-// const client = new Client( {
-//   host:"localhost",
-//   user: "postgres",
-//   port: 5432,
-//   password: "123456",
-//   database: "IRIS"
-// });
-
->>>>>>> refs/remotes/origin/main
->>>>>>> origin/main
 const client = new Client( {
   host:"localhost",
   user: "postgres",
   port: 5432,
-<<<<<<< HEAD
   password: "rimes@123",
   database: "IRIS"
-=======
-<<<<<<< HEAD
-  password: "iru@ADMIN123",
-  database: "iru_imd_db"
-=======
-  password: "12345",
-  database: "iris"
->>>>>>> refs/remotes/origin/main
->>>>>>> origin/main
 });
 // const client = new Client( {
 //   host:"localhost",
